4494d70787c97b7fbd37faa5f3936b724cacdaac
ZJovid <zavkovjov@gmail.com>
2022-06-19 15:33:18 +0500

    Page 404 with ErrorController

--- Web/Program.cs
+++ Web/Program.cs
@@ -61,6 +61,18 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.Use(async (ctx, next) =>
+{
+    await next();
 
+    if (ctx.Response.StatusCode == 404 && !ctx.Response.HasStarted)
+    {
+        //Re-execute the request so the user gets the error page
+        string? originalPath = ctx.Request.Path.Value;
+        ctx.Items["originalPath"] = originalPath;
+        ctx.Request.Path = "/error/404";
+        await next();
+    }
+});
 
 app.Run();
