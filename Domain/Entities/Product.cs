8f6e5393c9e526ca10aeb3135374d886923b4320
alijonizabiri <101598234+alijonizabiri@users.noreply.github.com>
2022-06-19 10:42:30 +0500

    Created 2 other table. Add References

--- Domain/Entities/Product.cs
+++ Domain/Entities/Product.cs
@@ -11,11 +11,13 @@ public class Product
     public string? Description { get; set; }
     [MaxLength(50)]
     public string? Sku { get; set; }
-    public int CategoryId { get; set; }
+    public int ProductCategoryId { get; set; }
     public ProductCategory? ProductCategory { get; set; }
-    public int InventoryId { get; set; }
+    public int ProductInventoryId { get; set; }
+    public ProductInventory? ProductInventory { get; set; }
     public decimal Price { get; set; }
     public int DiscountId { get; set; }
+    public Discount? Discount { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime ModifiedAt { get; set; }
     public DateTime DeletedAt { get; set; }
